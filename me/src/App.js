22795b7b43f94b955b5c9ec801ab43113e91736a
johngalimi <galimijohn@gmail.com>
2019-12-17 11:43:08 -0500

    switched to react-typist

--- me/src/App.js
+++ me/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {Typing} from 'react-typing';
-import './App.css';
+import Typist from 'react-typist';
+import '/home/galimijohn/me/me/node_modules/react-typist/dist/Typist.css';
 
 function App() {
-  return (
-	<div>
-		<Typing>
-	  		Hi, my name is John.
-		</Typing>
-	</div>
-  );
-}
+	return (
+		<div className = 'TypistWelcome'>
+			<Typist
+				className = 'TypistWelcome-main'
+			>
+				<span>Hi, my name is John.</span>
+				<br />
+				<div className = 'container'>
+					<p>I'm a full stack engineer based in Boston.</p>
+				</div>
+				Welcome to my website!	
+			</Typist>
+		</div>
+
+	);
+};
 
 export default App;
